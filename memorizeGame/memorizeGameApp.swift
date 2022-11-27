1795447fdc82d38d36033f307e665e01069258ad
searcheuchheang <searcheuchheang@gmail.com>
2022-11-27 20:25:30 +0700

    Initial Commit

---
+++ memorizeGame/memorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeGameApp.swift
+//  memorizeGame
+//
+//  Created by Macintosh on 27/11/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct memorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
